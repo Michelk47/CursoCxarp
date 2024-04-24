482805577900887218315b54b5408b35e00b912e
Michel de Alcantara <michel.alcantara@baraodemaua.br>
2024-04-24 17:08:05 -0300

    Aula 1

---
+++ hello/hello/Program.cs
@@ -0,0 +1,20 @@
+﻿using hello.Models;
+
+Pessoa p = new Pessoa();
+
+Pessoa pessoa1 = new Pessoa();  
+
+p.Nome = "Michel de Alcantarea";
+p.Idade = 25;
+
+pessoa1.Nome = "Diego Palareti";
+pessoa1.Idade = 100;
+
+DateTime dataAtual = DateTime.Now.AddDays(2);
+
+Console.WriteLine(dataAtual.ToString("dd/MM/yyyy HH:mm"));
+
+//pessoa1.Apresentar();
+//p.Apresentar();
+
+//p.Apresentar();
